0673fb2ff427b1a67947c6b820d55cbc7ad1d6c5
Konrad Rasmussen <MrSpring@mrspring.dk>
2014-09-30 13:27:09 +0200

    Cleaned up some code

--- src/main/java/dk/mrspring/kitchen/tileentity/TileEntityBoardRenderer.java
+++ src/main/java/dk/mrspring/kitchen/tileentity/TileEntityBoardRenderer.java
@@ -13,22 +13,16 @@ import java.util.List;
 @SideOnly(Side.CLIENT)
 public class TileEntityBoardRenderer extends TileEntitySpecialRenderer
 {
-	private List<ItemStack> layers;
-	private double yItemOffset = 0.0D;
-	
+
 	@Override
 	public void renderTileEntityAt(TileEntity var1, double x, double y, double z, float var8)
 	{
-		this.yItemOffset = 0.0D;
-		
 		GL11.glPushMatrix();
-		
-		GL11.glTranslated(x, y, z);
-		
-		TileEntityBoard tileEntity = (TileEntityBoard) var1;
-		
-		this.layers = tileEntity.getLayers();
 
+		TileEntityBoard tileEntity = (TileEntityBoard) var1;
+		GL11.glTranslated(x, y, z);
+
+		List<ItemStack> layers = tileEntity.getLayers();
 		SandwichRender.renderSandwich(layers);
 		
 		GL11.glPopMatrix();
