80f8b85fc53f917a330a15c0f4a110b09494555c
Konrad Rasmussen <MrSpring@mrspring.dk>
2014-10-06 10:10:17 +0200

    Rearranged Item variables

--- src/main/java/dk/mrspring/kitchen/KitchenItems.java
+++ src/main/java/dk/mrspring/kitchen/KitchenItems.java
@@ -2,10 +2,6 @@ package dk.mrspring.kitchen;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import dk.mrspring.kitchen.item.*;
-import dk.mrspring.kitchen.model.ModelBaconCooked;
-import dk.mrspring.kitchen.model.ModelBaconRaw;
-import dk.mrspring.kitchen.model.ModelBreadSliceBottom;
-import dk.mrspring.kitchen.model.ModelBreadSliceTop;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
@@ -16,32 +12,44 @@ public class KitchenItems
 {
 	// All the Item variables
 	public static final Item knife = new ItemKnife().setMaxStackSize(1);
+	public static final Item butter_knife = new ItemBase("butter_knife", true);
 	public static final Item mortar_and_pestle = new ItemMandP().setMaxStackSize(1);
+
 	public static final Item mortar = new ItemBase("mortar", true);
 	public static final Item pestle = new ItemBase("pestle", true);
-	public static final ItemSandwichable raw_bacon = new ItemSandwichable("bacon_raw", true, 1).setCustomModel(new ModelBaconRaw(), new ModelBaconRaw(), 1, 1);
-	public static final ItemSandwichBread bread_slice = (ItemSandwichBread) new ItemSandwichBread("bread_slice", true).setCustomModel(new ModelBreadSliceTop(), new ModelBreadSliceBottom(), 3, 2);
-	// The basic sandwich ItemStack. It's used as the icon fot the creative tab
-	public static ItemStack basic_sandwich = getSandwichItemStackWithNBTTags(new ItemStack[] { new ItemStack(bread_slice, 1, 0), new ItemStack(raw_bacon, 1, 0), new ItemStack(bread_slice, 1, 0) });
+
+	public static final Item raw_bacon = new ItemBase("bacon_raw",true);
+	public static final Item bacon = new ItemBase("bacon_cooked", true);
+
+	public static final Item bread_slice = new ItemBase("bread_slice", true);
+	public static final Item toast = new ItemBase("toast", true);
+
 	public static final Item tomato = new ItemTomato();
 	public static final Item lettuce = new ItemLettuce();
-	public static final ItemSandwichable tomato_slice = new ItemSandwichable("tomato_slice", true, 1);
-	public static final ItemSandwichable lettuce_leaf = new ItemSandwichable("lettuce_leaf", true, 2);
-	public static final ItemSandwichable bacon = new ItemSandwichable("bacon_cooked", true, 4).setCustomModel(new ModelBaconCooked(), new ModelBaconCooked(), 2, 2);
-	public static final ItemSandwichable potato_slice = new ItemSandwichable("potato_slice", true, 2);
-	public static final ItemSandwichable carrot_slice = new ItemSandwichable("carrot_slice", true, 2);
+	public static final Item tomato_slice = new ItemBase("tomato_slice", true);
+	public static final Item lettuce_leaf = new ItemBase("lettuce_leaf", true);
+
+	public static final Item potato_slice = new ItemBase("potato_slice", true);
+	public static final Item carrot_slice = new ItemBase("carrot_slice", true);
+
 	public static final Item flour = new ItemBase("flour", true);
-	public static final ItemSandwichBread toast = new ItemSandwichBread("toast", true);
-	public static final ItemSandwichable raw_roast_beef = new ItemSandwichable("raw_roast_beef", ModInfo.modid + ":beef_slice", true, 1);
-	public static final ItemSandwichable roast_beef = new ItemSandwichable("roast_beef", true, 4);
-	public static final ItemSandwichable raw_chicken_fillet = new ItemSandwichable("chicken_fillet_raw", true, 1);
-	public static final ItemSandwichable chicken_fillet = new ItemSandwichable("chicken_fillet_cooked", true, 4);
+
+	public static final Item raw_roast_beef = new ItemBase("raw_roast_beef", ModInfo.modid + ":beef_slice", true);
+	public static final Item roast_beef = new ItemBase("roast_beef", true);
+
+	public static final Item raw_chicken_fillet = new ItemBase("chicken_fillet_raw", true);
+	public static final Item chicken_fillet = new ItemBase("chicken_fillet_cooked", true);
+
 	public static final Item chicken_leg = new ItemFood(4, true).setUnlocalizedName("chicken_leg").setTextureName(ModInfo.modid + ":chicken_leg").setCreativeTab(Kitchen.instance.tab);
 	public static final Item cheese = new ItemFood(3, false).setUnlocalizedName("cheese").setTextureName(ModInfo.modid + ":cheese").setCreativeTab(Kitchen.instance.tab);
-	public static final ItemSandwichable cheese_slice = new ItemSandwichable("cheese_slice", true, 3);
+	public static final Item cheese_slice = new ItemBase("cheese_slice", true);
+
+	// The basic sandwich ItemStack. It's used as the icon fot the creative tab
+	public static ItemStack basic_sandwich = getSandwichItemStackWithNBTTags(new ItemStack[] { new ItemStack(bread_slice, 1, 0), new ItemStack(raw_bacon, 1, 0), new ItemStack(bread_slice, 1, 0) });
+
     public static final Item burnt_meat = new ItemFood(1, false).setUnlocalizedName("burnt_meat").setTextureName(ModInfo.modid + ":burnt_meat").setCreativeTab(Kitchen.instance.tab);
+
     public static final Item butter = new ItemBase("butter", true);
-    public static final Item butter_knife = new ItemBase("butter_knife", true);
 
 	// Pre-loads the sandwich ItemStack with some NBT-Data.
 	private static ItemStack getSandwichItemStackWithNBTTags(ItemStack[] layers)
