b2a2be11192681787d0fc54a1a336e618ab47740
MrSpring <mrspring@mrspring.dk>
2014-12-08 01:31:34 +0100

    Fixed #5

--- src/main/java/dk/mrspring/kitchen/ModConfig.java
+++ src/main/java/dk/mrspring/kitchen/ModConfig.java
@@ -5,53 +5,51 @@ import dk.mrspring.kitchen.config.*;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by MrSpring on 29-09-2014 for TheKitchenMod.
  */
 public class ModConfig
 {
-	private static BaseConfig[] configs;
+    private static BaseConfig[] configs;
 
-	public static void load(File baseFolder)
-	{
-		String base = baseFolder.getPath();
-		String[] names = new String[]{"Kitchen", "Knife", "Oven", "Sandwichable","Combo"};
-		configs = new BaseConfig[names.length];
+    public static void load(File baseFolder)
+    {
+        String base = baseFolder.getPath();
+        String[] names = new String[]{"Kitchen", "Knife", "Oven", "Sandwichable", "Combo"};
+        configs = new BaseConfig[names.length];
 
-		for (int i = 0; i < configs.length; i++)
-		{
-			try
-			{
-				Class configClass = Class.forName("dk.mrspring.kitchen.config." + names[i] + "Config");
-				String locationForJson = base + "\\TheKitchenMod\\" + names[i] + ".json";
-				ModLogger.print(ModLogger.INFO, "Loading " + names[i] + " from JSON @ " + locationForJson);
-				BaseConfig config = (BaseConfig) configClass.getConstructor(File.class, String.class).newInstance(new File(locationForJson), names[i]);
-				configs[i] = config.readFromFile();
-			} catch (ClassNotFoundException e)
-			{
-				e.printStackTrace();
-			} catch (NoSuchMethodException e)
-			{
-				e.printStackTrace();
-			} catch (InvocationTargetException e)
-			{
-				e.printStackTrace();
-			} catch (InstantiationException e)
-			{
-				e.printStackTrace();
-			} catch (IllegalAccessException e)
-			{
-				e.printStackTrace();
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-	/*
-		String configFolder = baseFolder.toURI().toASCIIString();
+        for (int i = 0; i < configs.length; i++)
+        {
+            try
+            {
+                Class configClass = Class.forName("dk.mrspring.kitchen.config." + names[i] + "Config");
+                String locationForJson = base + File.separator + "TheKitchenMod" + File.separator + names[i] + ".json";
+                ModLogger.print(ModLogger.INFO, "Loading " + names[i] + " from JSON @ " + locationForJson);
+                BaseConfig config = (BaseConfig) configClass.getConstructor(File.class, String.class).newInstance(new File(locationForJson), names[i]);
+                configs[i] = config.readFromFile();
+            } catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e)
+            {
+                e.printStackTrace();
+            } catch (InvocationTargetException e)
+            {
+                e.printStackTrace();
+            } catch (InstantiationException e)
+            {
+                e.printStackTrace();
+            } catch (IllegalAccessException e)
+            {
+                e.printStackTrace();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    /*
+        String configFolder = baseFolder.toURI().toASCIIString();
 		config = new KitchenConfig(new File(configFolder + "\\TheKitchenMod.json"));
 		knifeConfig = new KnifeConfig(new File(configFolder + "\\KnifeRecipe.json"));
 
@@ -85,22 +83,22 @@ public class ModConfig
 			config = new KitchenConfig(oldFile);
 		}
 	*/
-	}
+    }
 
-	public static KitchenConfig getKitchenConfig()
-	{
-		return (KitchenConfig) configs[0];
-	}
+    public static KitchenConfig getKitchenConfig()
+    {
+        return (KitchenConfig) configs[0];
+    }
 
-	public static KnifeConfig getKnifeConfig()
-	{
-		return (KnifeConfig) configs[1];
-	}
+    public static KnifeConfig getKnifeConfig()
+    {
+        return (KnifeConfig) configs[1];
+    }
 
-	public static OvenConfig getOvenConfig()
-	{
-		return (OvenConfig) configs[2];
-	}
+    public static OvenConfig getOvenConfig()
+    {
+        return (OvenConfig) configs[2];
+    }
 
     public static SandwichableConfig getSandwichConfig()
     {
