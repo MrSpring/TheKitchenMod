bfa1e7a2d5b0212348a7992bed18ecf5a9eca95f
MrSpring <mrspring@mrspring.dk>
2014-12-09 23:53:14 +0100

    Added name based on metadata

--- src/main/java/dk/mrspring/kitchen/item/ItemMixingBowl.java
+++ src/main/java/dk/mrspring/kitchen/item/ItemMixingBowl.java
@@ -1,5 +1,8 @@
 package dk.mrspring.kitchen.item;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+
 /**
  * Created by MrSpring on 09-12-2014 for TheKitchenMod.
  */
@@ -8,5 +11,13 @@ public class ItemMixingBowl extends ItemBase
     public ItemMixingBowl(String name)
     {
         super(name, true);
+    }
+
+    @Override
+    public String getItemStackDisplayName(ItemStack itemStack)
+    {
+        if (itemStack.getItemDamage() == 0)
+            return StatCollector.translateToLocal("item.mixing_bowl.empty.name");
+        else return StatCollector.translateToLocal("item.mixing_bowl.fill.name");
     }
 }
