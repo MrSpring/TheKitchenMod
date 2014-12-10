050abf3ab8a22ee7a8c0c9cb291fd623925d2319
MrSpring <mrspring@mrspring.dk>
2014-12-10 01:36:19 +0100

    Added Mixing Bowl icons and language

--- src/main/java/dk/mrspring/kitchen/item/ItemMixingBowl.java
+++ src/main/java/dk/mrspring/kitchen/item/ItemMixingBowl.java
@@ -1,6 +1,8 @@
 package dk.mrspring.kitchen.item;
 
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.util.StatCollector;
 
 /**
@@ -8,9 +10,13 @@ import net.minecraft.util.StatCollector;
  */
 public class ItemMixingBowl extends ItemBase
 {
+    IIcon[] icons;
+
     public ItemMixingBowl(String name)
     {
         super(name, true);
+        this.setMaxDamage(3);
+        this.setMaxStackSize(1);
     }
 
     @Override
@@ -19,5 +25,22 @@ public class ItemMixingBowl extends ItemBase
         if (itemStack.getItemDamage() == 0)
             return StatCollector.translateToLocal("item.mixing_bowl.empty.name");
         else return StatCollector.translateToLocal("item.mixing_bowl.full.name");
+    }
+
+    @Override
+    public void registerIcons(IIconRegister register)
+    {
+        icons = new IIcon[4];
+
+        for (int i = 0; i < icons.length; i++)
+            icons[i] = register.registerIcon(this.iconString + "_" + i);
+    }
+
+    @Override
+    public IIcon getIconFromDamage(int metadata)
+    {
+        if (metadata < 4 && metadata > 0)
+            return this.icons[metadata];
+        else return this.icons[0];
     }
 }
