725e3e02a6952d22d8fd5c382be6e53c70bc32ac
MrSpring <mrspring@mrspring.dk>
2015-04-20 02:01:40 +0200

    Added missing WAILA functions

--- src/main/java/dk/mrspring/kitchen/comp/waila/WailaOvenProvider.java
+++ src/main/java/dk/mrspring/kitchen/comp/waila/WailaOvenProvider.java
@@ -4,8 +4,12 @@ import dk.mrspring.kitchen.tileentity.TileEntityOven;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -55,6 +59,11 @@ public class WailaOvenProvider implements IWailaDataProvider
 
     @Override
     public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        return null;
+    }
+
+    public NBTTagCompound getNBTData(EntityPlayerMP entityPlayerMP, TileEntity tileEntity, NBTTagCompound nbtTagCompound, World world, int i, int i1, int i2)
     {
         return null;
     }
--- src/main/java/dk/mrspring/kitchen/comp/waila/WailaToasterProvider.java
+++ src/main/java/dk/mrspring/kitchen/comp/waila/WailaToasterProvider.java
@@ -4,8 +4,12 @@ import dk.mrspring.kitchen.tileentity.TileEntityToaster;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -54,6 +58,11 @@ public class WailaToasterProvider implements IWailaDataProvider
 
     @Override
     public List<String> getWailaTail(ItemStack itemStack, List<String> list, IWailaDataAccessor iWailaDataAccessor, IWailaConfigHandler iWailaConfigHandler)
+    {
+        return null;
+    }
+
+    public NBTTagCompound getNBTData(EntityPlayerMP entityPlayerMP, TileEntity tileEntity, NBTTagCompound nbtTagCompound, World world, int i, int i1, int i2)
     {
         return null;
     }
