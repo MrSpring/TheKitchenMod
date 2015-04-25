7eadf570781a88f758c4e3c279259b9e2fd3a4ea
MrSpring <mrspring@mrspring.dk>
2015-04-25 02:15:29 +0200

    Updated to newer versions of WAILA

--- src/main/java/dk/mrspring/kitchen/comp/waila/WailaDataProvider.java
+++ src/main/java/dk/mrspring/kitchen/comp/waila/WailaDataProvider.java
@@ -7,7 +7,11 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -38,6 +42,11 @@ public class WailaDataProvider implements IWailaDataProvider
     public List<String> getWailaTail(ItemStack itemStack, List<String> strings, IWailaDataAccessor iWailaDataAccessor, IWailaConfigHandler iWailaConfigHandler)
     {
         return strings;
+    }
+
+    public NBTTagCompound getNBTData(EntityPlayerMP entityPlayerMP, TileEntity tileEntity, NBTTagCompound nbtTagCompound, World world, int i, int i1, int i2)
+    {
+        return null;
     }
 
     public static void callbackRegister(IWailaRegistrar registrar)
