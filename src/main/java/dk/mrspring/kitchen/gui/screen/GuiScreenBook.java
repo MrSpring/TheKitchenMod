5d56ab9fc536270fa83eba9f79d77634f9d135c1
MrSpring <mrspring@mrspring.dk>
2015-04-26 17:27:22 +0200

    Working on new code stuff

--- src/main/java/dk/mrspring/kitchen/gui/screen/GuiScreenBook.java
+++ src/main/java/dk/mrspring/kitchen/gui/screen/GuiScreenBook.java
@@ -1,21 +1,23 @@
 package dk.mrspring.kitchen.gui.screen;
 
-import dk.mrspring.kitchen.*;
+import com.google.gson.Gson;
+import dk.mrspring.kitchen.Kitchen;
+import dk.mrspring.kitchen.ModConfig;
+import dk.mrspring.kitchen.ModInfo;
+import dk.mrspring.kitchen.config.wrapper.JsonItemStack;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
-import scala.actors.threadpool.Arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -109,8 +111,6 @@ public class GuiScreenBook extends GuiScreen
             minecraft.getTextureManager().bindTexture(location);
             int x = (maxWidth / 2) - (width / 2);
             drawTexturedModalRect(x, 2, u, v, width, height);
-//            drawRect(0, 0, width, height, 0xFFFFFF);
-//            minecraft.fontRenderer.drawString("IMAGE", 0, 0, 0x4C1C06);
             GL11.glPopMatrix();
         }
 
@@ -364,7 +364,7 @@ public class GuiScreenBook extends GuiScreen
 
         this.addTableOfContent();
 
-        pageIndex = new int[]{ // TODO: Finish book.
+        /*pageIndex = new int[]{ // TODO: Finish book.
                 this.addChapter("item.cooking_book.pages.sandwiches.title", 0, 99, 0,
                         START_TEXT,
                         "item.cooking_book.pages.sandwiches.text01",
@@ -460,7 +460,7 @@ public class GuiScreenBook extends GuiScreen
                 this.addChapter("item.cooking_book.pages.timer.title", 1, 99, 0,
                         START_TEXT,
                         "item.cooking_book.pages.timer.text01",
-                        STOP_TEXT)};
+                        STOP_TEXT)};*/
     }
 
     private String translate(String text)
@@ -476,18 +476,73 @@ public class GuiScreenBook extends GuiScreen
         return translated;
     }
 
-    private static final String START_CRAFTING = "%C_START%"; // TODO: Replace with enum?
-    private static final String STOP_CRAFTING = "%C_STOP%";
+    private static final String START_CRAFTING = "$C_START$"; // TODO: Replace with enum?
+    private static final String STOP_CRAFTING = "$C_STOP$";
 
-    private static final String START_TEXT = "%T_START%";
-    private static final String STOP_TEXT = "%T_STOP%";
-    private static final String CENTER_TEXT = "%T_CENTER%";
-    private static final String DONT_TRANSLATE_TEXT = "%T_DT%";
+    private static final String START_TEXT = "$T_START$";
+    private static final String STOP_TEXT = "$T_STOP$";
+    private static final String CENTER_TEXT = "$T_CENTER$";
+    private static final String DONT_TRANSLATE_TEXT = "$T_DT$";
 
-    private static final String START_IMAGE = "%I_START%";
-    private static final String STOP_IMAGE = "%I_STOP%";
+    private static final String START_IMAGE = "$I_START$";
+    private static final String STOP_IMAGE = "$I_STOP$";
 
-    private static final String ADD_SPACE = "%SPACE%";
+    private static final String ADD_SPACE = "$SPACE$";
+
+    private Object[] convertStringToObjects(String string)
+    {
+        List<Object> objects = new ArrayList<Object>();
+        String[] split = string.split("$");
+        Iterator<String> iterator = Arrays.asList(split).iterator();
+        while (iterator.hasNext())
+        {
+            String next = iterator.next();
+            if (next.equals(START_CRAFTING))
+            {
+                String recipe = iterator.next();
+                String[] fullRecipe = recipe.split(",");
+                ItemStack[] actualRecipe = new ItemStack[fullRecipe.length];
+                try
+                {
+                    Gson gson = new Gson();
+                    JsonItemStack output = gson.fromJson(fullRecipe[0], JsonItemStack.class);
+                    actualRecipe[0] = output.toItemStack();
+                    for (int i = 1; i < fullRecipe.length; i++)
+                    {
+                        String stack = fullRecipe[i];
+                        if (stack.equals("null"))
+                            actualRecipe[i] = null;
+                        else
+                        {
+                            JsonItemStack jsonStack = gson.fromJson(stack, JsonItemStack.class);
+                            if (jsonStack != null)
+                                actualRecipe[i] = jsonStack.toItemStack();
+                        }
+                    }
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            } else if (next.equals(START_TEXT))
+            {
+
+            } else if (next.equals(START_IMAGE))
+            {
+
+            } else if (next.equals(ADD_SPACE))
+            {
+                String spaceAmount = iterator.next();
+                try
+                {
+                    int height = Integer.valueOf(spaceAmount);
+                    objects.add(new SpacerElement(height));
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     private int addChapter(String title, int textureIndex, int logoU, int logoV, Object... content)
     {
@@ -638,16 +693,16 @@ public class GuiScreenBook extends GuiScreen
 
     private void addModRepostInfo()
     {
-        List<String> lines = new ArrayList<String>();
-
-        lines.add(translate("item.cooking_book.pages.stop_mod_reposts.title"));
-        lines.add("");
-        lines.add(translate("item.cooking_book.pages.stop_mod_reposts.text"));
-
-        List<Element> elements = new ArrayList<Element>();
-        elements.add(new TextElement(lines).center());
-
-        pages.addAll(splitElementsToPages(elements, mc, 120));
+//        List<String> lines = new ArrayList<String>();
+//
+//        lines.add(translate("item.cooking_book.pages.stop_mod_reposts.title"));
+//        lines.add("");
+//        lines.add(translate("item.cooking_book.pages.stop_mod_reposts.text"));
+//
+//        List<Element> elements = new ArrayList<Element>();
+//        elements.add(new TextElement(lines).center());
+//
+//        pages.addAll(splitElementsToPages(elements, mc, 120));
     }
 
     private void addIntroPages()
@@ -657,7 +712,8 @@ public class GuiScreenBook extends GuiScreen
         if (!Kitchen.proxy.versionHighlights.equals(""))
         {
             lines.add("\n\n" + translate("item.cooking_book.pages.update_highlights"));
-            lines.add(Kitchen.proxy.versionHighlights);
+            lines.add(Kitchen.proxy.versionHighlights.replace("Â§", "§"));
+            System.out.println("Added line: " + Kitchen.proxy.versionHighlights);
         }
 
         List<Element> introElements = new ArrayList<Element>();
@@ -732,7 +788,6 @@ public class GuiScreenBook extends GuiScreen
         if (pageTemp.size() > 0)
             localPages.add(new SimplePage(new ArrayList<Element>(pageTemp)));
 
-//        System.out.println("localPages.size() = " + localPages.size());
         return localPages;
     }
 
@@ -793,311 +848,4 @@ public class GuiScreenBook extends GuiScreen
     {
         return mouseX >= posX && mouseY >= posY && mouseX < posX + width && mouseY < posY + height;
     }
-    
-    /*public static int drawSplitCenteredString(FontRenderer renderer, String s, int x, int y, int maxLength, int color, boolean shadow)
-    {
-        List<String> lines = renderer.listFormattedStringToWidth(s, maxLength);
-
-        for (int i = 0; i < lines.size(); i++)
-        {
-            String line = lines.get(i);
-            int lineWidth = renderer.getStringWidth(line);
-            renderer.drawString(line, x - (lineWidth / 2), y + (i * 9), color, shadow);
-        }
-
-        return lines.size();
-    }
-
-    List<Page> pages = new ArrayList<Page>();
-
-    int pageSet = 0;
-
-    @Override
-    public void initGui()
-    {
-        super.initGui();
-
-        if (pages != null)
-            pages.clear();
-        else pages = new ArrayList<Page>();
-
-        this.addIntroPages();
-
-        if (ModConfig.getKitchenConfig().show_mod_repost_info)
-            this.addModRepostPages();
-
-
-    }
-
-    private void addOtherPages()
-    {
-        Category sandwiches, oven, plate, pan, jam, toaster, waffleIron;
-
-
-    }
-
-    private Category getSandwichCategory()
-    {
-        Category category = new Category();
-
-        StringBuilder builder = new StringBuilder();
-        builder.append("§lSandwiches:§r\n");
-        builder.append(StatCollector.translateToLocal(""));
-
-        return category;
-    }
-
-    private void addModRepostPages()
-    {
-        List<String> stopModRepostsInfo = new ArrayList<String>();
-        stopModRepostsInfo.add(StatCollector.translateToLocal("item.cooking_book.pages.stop_mod_reposts.title"));
-        stopModRepostsInfo.addAll(mc.fontRenderer.listFormattedStringToWidth(StatCollector.translateToLocal("item.cooking_book.pages.stop_mod_reposts.text").replace("\\n", "\n"), 120));
-        this.pages.addAll(getPagesFromLines(stopModRepostsInfo));
-    }
-
-    private void addIntroPages()
-    {
-        String introduction = StatCollector.translateToLocal("item.cooking_book.pages.introduction").replace("\\n", "\n").replace("%v", ModInfo.version) + ".";
-
-        if (!Kitchen.proxy.versionHighlights.equals(""))
-        {
-            introduction += "\n\n" + StatCollector.translateToLocal("item.cooking_book.pages.update_highlights") + "\n\n";
-            introduction += Kitchen.proxy.versionHighlights;
-        }
-
-        List<String> introLines = mc.fontRenderer.listFormattedStringToWidth(introduction, 120);
-
-        final List<String> pageOneLines = introLines;
-
-        pages.add(new Page()
-        {
-            List<String> lines;
-
-            {
-                if (pageOneLines.size() > 13)
-                {
-                    this.lines = pageOneLines.subList(0, 13);
-                } else this.lines = pageOneLines;
-            }
-
-            @Override
-            public void draw(Minecraft minecraft, int width)
-            {
-                GL11.glPushMatrix();
-                GL11.glScalef(1.5F, 1.5F, 1.5F);
-                minecraft.fontRenderer.drawString("Introduction", width / 3 - (minecraft.fontRenderer.getStringWidth("Introduction") / 2), 0, 0x4C1C06, false);
-                GL11.glPopMatrix();
-                for (int i = 0; i < lines.size(); i++)
-                {
-                    String line = lines.get(i);
-                    int lineWidth = minecraft.fontRenderer.getStringWidth(line);
-                    minecraft.fontRenderer.drawString(line, width / 2 - (lineWidth / 2), 20 + (i * 9), 0x4C1C06, false);
-                }
-            }
-        });
-
-        if (introLines.size() > 13)
-            this.pages.add(new SimplePage(new TextPageElement(introLines.subList(14, introLines.size()))).center());
-    }
-
-    private List<Page> getPagesFromLines(List<String> lines)
-    {
-        List<Page> localPages = new ArrayList<Page>();
-        List<String> pageTemp = new ArrayList<String>();
-
-        for (String line : lines)
-        {
-            System.out.println("Going through line: " + line + ", current pages: " + pages.size() + ", current page size: " + pageTemp.size());
-            pageTemp.add(line);
-            if (pageTemp.size() > 15)
-            {
-                localPages.add(new SimplePage(new TextPageElement(new ArrayList<String>(pageTemp))));
-                System.out.println("Added a new Page!");
-                pageTemp.clear();
-            }
-        }
-        if (pageTemp.size() > 0)
-            localPages.add(new SimplePage(new TextPageElement(pageTemp)));
-        return localPages;
-    }
-
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float p_73863_3_)
-    {
-        super.drawScreen(mouseX, mouseY, p_73863_3_);
-
-        Page leftPage = null, rightPage = null;
-
-        leftPage = pages.get(pageSet * 2);
-        if (pages.size() > (pageSet * 2) + 1)
-            rightPage = pages.get((pageSet * 2) + 1);
-
-        mc.getTextureManager().bindTexture(new ResourceLocation("kitchen", "textures/gui/cooking_book_left.png"));
-        drawTexturedModalRect((width / 2) - 140, 20, 0, 0, 140, 180);
-        drawTexturedModalRect((width / 2) - 140 + 14, 190, isMouseHovering(mouseX, mouseY, (width / 2) - 140 + 14, 190, 24, 24) ? 24 : 0, 180, 24, 24);
-        mc.getTextureManager().bindTexture(new ResourceLocation("kitchen", "textures/gui/cooking_book_right.png"));
-        drawTexturedModalRect(width / 2, 20, 0, 0, 140, 180);
-        drawTexturedModalRect((width / 2) + 140 - 38, 190, isMouseHovering(mouseX, mouseY, (width / 2) + 140 - 38, 190, 24, 24) ? 24 : 0, 180, 24, 24);
-
-
-        if (leftPage != null)
-        {
-            mc.fontRenderer.drawString(String.valueOf(pageSet * 2 + 1), (width / 2) - 70, 182, 0x4C1C06, false);
-            GL11.glPushMatrix();
-            GL11.glTranslatef((width / 2) - 127, 35, 0);
-            leftPage.draw(mc, 120);
-            GL11.glPopMatrix();
-        }
-        if (rightPage != null)
-        {
-            mc.fontRenderer.drawString(String.valueOf(pageSet * 2 + 2), (width / 2) + 70, 182, 0x4C1C06, false);
-            GL11.glPushMatrix();
-            GL11.glTranslatef(*//*(width / 2) + 7*//*0, 35, 0);
-            rightPage.draw(mc, 120);
-            GL11.glPopMatrix();
-        }
-    }
-
-    @Override
-    protected void mouseClicked(int mouseX, int mouseY, int button)
-    {
-        if (isMouseHovering(mouseX, mouseY, (width / 2) + 140 - 38, 190, 24, 24) && (pageSet + 1) * 2 < pages.size())
-            this.pageSet++;
-        else if (isMouseHovering(mouseX, mouseY, (width / 2) - 140 + 14, 190, 24, 24) && pageSet > 0)
-            this.pageSet--;
-    }
-
-    static boolean isMouseHovering(int mouseX, int mouseY, int posX, int posY, int width, int height)
-    {
-        return mouseX >= posX && mouseY >= posY && mouseX < posX + width && mouseY < posY + height;
-    }
-
-    private class Category
-    {
-        int startPage;
-        List<Page> pages;
-
-        public Category()
-        {
-            this(0, new ArrayList<Page>());
-        }
-
-        public Category(int start, List<Page> pages)
-        {
-            this.startPage = start;
-            this.pages = pages;
-        }
-
-        public int getStartPage()
-        {
-            return this.startPage;
-        }
-
-        public int getEndPage()
-        {
-            return this.startPage + pages.size();
-        }
-
-        public List<Page> getPages()
-        {
-            return this.pages;
-        }
-
-        public void setStartPage(int startPage)
-        {
-            this.startPage = startPage;
-        }
-
-        public void setPages(List<Page> pages)
-        {
-            this.pages = pages;
-        }
-
-        public void addPage(Page page)
-        {
-            this.pages.add(page);
-        }
-    }
-
-    private interface PageElement
-    {
-        public void draw(Minecraft minecraft, int maxWidth);
-
-        public int getHeight(Minecraft minecraft, int maxWidth);
-    }
-
-    private class TextPageElement implements PageElement
-    {
-        List<String> lines;
-        public boolean center;
-
-        public TextPageElement(List<String> lines)
-        {
-            this.lines = lines;
-        }
-
-        @Override
-        public void draw(Minecraft minecraft, int maxWidth)
-        {
-            for (int i = 0; i < this.lines.size(); i++)
-            {
-                String line = this.lines.get(i);
-                int lineWidth = minecraft.fontRenderer.getStringWidth(line);
-                minecraft.fontRenderer.drawString(line, width / 2 - (lineWidth / 2), (i * 9), 0x4C1C06, false);
-            }
-        }
-
-        @Override
-        public int getHeight(Minecraft minecraft, int maxWidth)
-        {
-            return lines.size() * 9;
-        }
-    }
-
-    private interface Page
-    {
-        public void draw(Minecraft minecraft, int width);
-    }
-
-    private class SimplePage implements Page
-    {
-        List<PageElement> lines;
-
-        public SimplePage(PageElement element)
-        {
-            List<PageElement> elements = new ArrayList<PageElement>();
-            elements.add(element);
-            this.lines = elements;
-        }
-
-        public SimplePage(List<PageElement> lines)
-        {
-            this.lines = lines;
-
-//            System.out.println("Creating new Text Page containing these lines: ");
-//            for (String line : lines)
-//                System.out.println(line);
-        }
-
-        public SimplePage center()
-        {
-            for (PageElement element : lines)
-                if (element instanceof TextPageElement)
-                    ((TextPageElement) element).center = true;
-            return this;
-        }
-
-        @Override
-        public void draw(Minecraft minecraft, int width)
-        {
-            GL11.glPushMatrix();
-            for (PageElement element : lines)
-            {
-                element.draw(minecraft, width);
-                int height = element.getHeight(minecraft, width);
-//                GL11.glTranslatef(0, height, 0);
-            }
-            GL11.glPopMatrix();
-        }
-    }*/
 }
