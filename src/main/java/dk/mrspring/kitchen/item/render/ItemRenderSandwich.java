6e9a93701692ae6703e3be64dc21cfd98de2724a
MrSpring <mrspring@mrspring.dk>
2016-06-01 00:01:44 +0200

    Scaled sandwich when rendering on plate

--- src/main/java/dk/mrspring/kitchen/item/render/ItemRenderSandwich.java
+++ src/main/java/dk/mrspring/kitchen/item/render/ItemRenderSandwich.java
@@ -2,11 +2,10 @@ package dk.mrspring.kitchen.item.render;
 
 import dk.mrspring.kitchen.ModConfig;
 import dk.mrspring.kitchen.config.ClientConfig;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
 
 import static dk.mrspring.kitchen.ClientUtils.rotate;
+import static dk.mrspring.kitchen.ClientUtils.scale;
 import static dk.mrspring.kitchen.ClientUtils.translate;
 
 public class ItemRenderSandwich extends ItemRenderer
@@ -34,6 +33,12 @@ public class ItemRenderSandwich extends ItemRenderer
     @Override
     public void renderAnyTypes(ItemStack item, Object... data)
     {
+        if (item.hasTagCompound() && item.getTagCompound().hasKey(PlateRender.IS_RENDERING_ON_PLATE))
+        {
+            rotate(-90, 1, 0, 0);
+            translate(0F, -0.05F, 0.255F);
+            scale(2F);
+        }
         SandwichRender.renderSandwich(new SandwichRender.Sandwich(item));
     }
 
@@ -42,91 +47,4 @@ public class ItemRenderSandwich extends ItemRenderer
     {
         return ModConfig.getClientConfig().sandwich_rendering;
     }
-/*
-    @Override
-    public boolean handleRenderType(ItemStack item, ItemRenderType type)
-    {
-        switch (type)
-        {
-            case EQUIPPED:
-                return true;
-            case EQUIPPED_FIRST_PERSON:
-                return true;
-            case ENTITY:
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    @Override
-    public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper)
-    {
-        switch (helper)
-        {
-            case ENTITY_BOBBING:
-            case ENTITY_ROTATION:
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    @Override
-    public void renderItem(ItemRenderType type, ItemStack item, Object... data)
-    {
-        switch (type)
-        {
-            case EQUIPPED:
-            {
-                GL11.glPushMatrix();
-
-                GL11.glRotatef(-60, 1, 0, 0);
-                GL11.glRotatef(20, 0, 0, 1);
-                GL11.glTranslatef(.4F, 1.2F, .125F);
-                float f = 1.1F;
-                GL11.glScalef(f, f, f);
-
-                SandwichRender.renderSandwich(new SandwichRender.Sandwich(item));
-
-                GL11.glPopMatrix();
-
-                break;
-            }
-            case EQUIPPED_FIRST_PERSON:
-            {
-                GL11.glPushMatrix();
-
-                //GL11.glTranslatef(-1F, 0F, 0F);
-                GL11.glTranslatef(.35F, 1.3F, -0.2F);
-                GL11.glRotatef(90, 0, 1, 0);
-                GL11.glRotatef(-10, 1, 0, 0);
-
-                float scale = 0.7F;
-                GL11.glScalef(scale, scale, scale);
-
-                SandwichRender.renderSandwich(new SandwichRender.Sandwich(item));
-
-                GL11.glPopMatrix();
-
-                break;
-            }
-            case ENTITY:
-            {
-                GL11.glPushMatrix();
-
-                GL11.glTranslatef(0F, 2F, 0F);
-                float f = 1.5F;
-                GL11.glScalef(f, f, f);
-
-
-                SandwichRender.renderSandwich(new SandwichRender.Sandwich(item));
-
-                GL11.glPopMatrix();
-
-                break;
-            }
-        }
-    }
-*/
 }
